006f0ed11438791864e120e544727dc5630d6f4e
Ankichi <59509443+An39nA0219@users.noreply.github.com>
2021-10-25 11:30:03 +0900

    プロジェクト ファイルを追加します。

---
+++ MvcMovie3/Data/MvcMovie3Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie3.Models;
+
+namespace MvcMovie3.Data
+{
+    public class MvcMovie3Context : DbContext
+    {
+        public MvcMovie3Context (DbContextOptions<MvcMovie3Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMovie3.Models.Movie> Movie { get; set; }
+    }
+}
